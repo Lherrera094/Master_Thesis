92d30c1099e5f594f542538240a3d53142c874e0
Luis Carlos Herrera Quesada <127136402+Lherrera094@users.noreply.github.com>
2023-05-02 06:07:15 -0600

    Add files via upload

--- FAR3d Results Visualization/output_eigenfunctions_TK.py
+++ FAR3d Results Visualization/output_eigenfunctions_TK.py
@@ -16,7 +16,6 @@ from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 from PIL import Image
 import os
-from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
 #Read all folders and create the folder to save the plots
@@ -37,6 +36,12 @@ def create_files():
     txt = list(filter(lambda main_file: ".txt" in main_file, main_file))[0]
     
     return results,file,txt,saving_file
+
+def sort_key(item):
+    parts = item.split('.png')
+    first, last = parts[0].split('_')
+    return -float(last), float(first)
+
 
 #Remove not used files for each folder
 def remove_files(direct):
@@ -109,11 +114,24 @@ def plasma_parameters(directory,profiles,frec):
 def get_main_data(f):
     data = open(f+'/farprt')
     ndata = data.readlines()
+    line_diference = 4
+    toroidal_couplings = []
+    
     tline = [idx for idx,line in enumerate(ndata) if 'n       Avg. gam:         Avg. om_r:' in line][0] + 1
     n, grwth, omega = ndata[tline].split()
     grwth, omega = float(grwth), float(omega)
+    toroidal_couplings.append(n)
     
-    return n, grwth, omega
+    try:
+        for i in range(1,10):
+            tline = [idx for idx,line in enumerate(ndata) if 'n       Avg. gam:         Avg. om_r:' in line][0] + 1 + (line_diference*i)
+            n, grwth1, omega1 = ndata[tline].split()
+            toroidal_couplings.append(n)
+            
+    except Exception as e:
+        x = 0
+    
+    return grwth, omega, toroidal_couplings
 
 
 #Print file important data
@@ -175,43 +193,74 @@ def get_values(data_frame):
     return dominant_mode, radial_pos, dominant_mode_2, radial_pos_2,dominant_mode_3, radial_pos_3, alfmode,x1,x2
 
 
-#Asing diferent colors for the different toroidal families
-def get_colors(n_family):    
-    n_family = int(n_family)
+def get_colors_dict(n):
+    n = int(n)
     
-    if n_family == 5:
-        fam = [5,9,13,17]
-        colfam = ["blue","green","mediumpurple","orange"]
-        colors = ["navy","blue","dodgerblue", 
-                  "darkgreen","forestgreen","seagreen","green",
-                  "indigo","darkviolet","mediumpurple","magenta","violet",
-                  "goldenrod","darkorange","orange","gold","yellow"]
-    if n_family == 6:
-        fam = [6,10,14]
-        colfam = ["darkturquoise","red","forestgreen"]
-        colors = ["darkcyan","cyan","darkturquoise", 
-                  "firebrick","crimson","red","tomato",
-                  "darkgreen","forestgreen","seagreen","green","limegreen"]  
+    d =  {
+        3: {
+            'colfam': 'k',
+            'colors': ['black', 'dimgrey', 'grey']             
+        },
         
-    if n_family == 3 or n_family==7:
-        fam = [3,7,11,15]
-        colfam = ["k","limegreen","mediumpurple","orange"]
-        colors = ["black","dimgrey","grey", 
-                  "darkgreen","forestgreen","limegreen","lime",
-                  "indigo","darkviolet","mediumpurple","magenta","violet",
-                  "goldenrod","darkorange","orange","gold","yellow"]
+        5: {
+            'colfam': 'blue',
+            'colors': ['navy', 'blue', 'dodgerblue']
+        },
+        6: {
+            'colfam': 'darkturquoise',
+            'colors': ['darkcyan', 'cyan', 'darkturquoise']
+        },
         
-    if n_family == 8:
-        fam = [8,12,16]
-        colfam = ["limegreen","mediumpurple","orange"]
-        colors = ["firebrick","crimson","red","tomato",
-                  "indigo","darkviolet","mediumpurple","magenta","violet",
-                  "goldenrod","darkorange","orange","gold","yellow"]
+        7: {
+            'colfam': 'limegreen',
+            'colors': ['darkgreen', 'forestgreen', 'limegreen', 'lime']
+        },
+        8: {
+            'colfam': 'red',
+            'colors': ['firebrick', 'crimson', 'red', 'tomato']
+        },
+        9: {
+            'colfam': "green",
+            'colors': ["darkgreen","forestgreen","seagreen","green"] 
+        },
+        10: {
+            'colfam': "red",
+            'colors': ["firebrick","crimson","red","tomato"] 
+        },
+        11: {
+            'colfam': "mediumpurple",
+            'colors': ["indigo","darkviolet","mediumpurple","magenta","violet"] 
+        },
+        12: {
+            'colfam': 'k',
+            'colors': ['black', 'dimgrey', 'grey']  
+        },
+        13: {
+            'colfam': "mediumpurple",
+            'colors': ["indigo","darkviolet","mediumpurple","magenta","violet"] 
+        },
+        14: {
+            'colfam': "forestgreen",
+            'colors': ["darkgreen","forestgreen","seagreen","green","limegreen"] 
+        },
+        15: {
+            'colfam': "orange",
+            'colors': ["goldenrod","darkorange","orange","gold","yellow"] 
+        },
+        16: {
+            'colfam': 'blue',
+            'colors': ['navy', 'blue', 'dodgerblue']
+        },
+        17: {
+            'colfam': "orange",
+            'colors': ["goldenrod","darkorange","orange","gold","yellow"] 
+        },
+    }
     
-    return fam, colfam, colors
+    return d[n]
 
 
-def plot_eigenfunctions(dm,dm2,dm3,alfm,rp,rp2,rp3,df,r,family,n_fam,colfam,colors,energy,beta,f,sav_file):
+def plot_eigenfunctions(dm,dm2,dm3,alfm,rp,rp2,rp3,df,r,energy,beta,f,sav_file,tor_coupl):
     im = plt.figure(figsize=(9,8))
     i,j,k=0,0,0
     
@@ -220,8 +269,8 @@ def plot_eigenfunctions(dm,dm2,dm3,alfm,rp,rp2,rp3,df,r,family,n_fam,colfam,colo
     plt.annotate(f"Coupled: {dm2}", xy=(0.01, 0.305), xycoords='axes fraction', fontsize = 15)             
     plt.annotate(f"Alfvén Mode: {alfm}", xy=(0.01, 0.275), xycoords='axes fraction', fontsize = 15)
                          
-        
-    if(int(n_fam[0])==6):
+    #Marking lines and annotations    
+    if 6 in tor_coupl:
         plt.axvline(rp/1000,color="blue",linewidth=1)
             
     else:
@@ -231,67 +280,85 @@ def plot_eigenfunctions(dm,dm2,dm3,alfm,rp,rp2,rp3,df,r,family,n_fam,colfam,colo
         plt.axvline(rp3/1000,color="k",linestyle="--",linewidth=1)
         plt.annotate(f"2nd Coupled: {dm3}", xy=(0.01, 0.245), xycoords='axes fraction', fontsize = 15)                 
 
-    for n in family:
-        plt.axhline(0,xmin = 0.05, xmax = 0.07,color=colfam[k],linewidth=2,label=f"n= {n}")                   
+    for n in tor_coupl:
+        d = get_colors_dict(n)
+        plt.axhline(0,xmin = 0.05, xmax = 0.06,color=d["colfam"],linewidth=2,label=f"n= {n}")                   
         k += 1
                 
     plt.axvline(rp2/1000,color="k",linewidth=1)
     
-    for col in df.columns:
-        if "I" in col:
-            plt.plot(r,df[col],"--",color=colors[i],linewidth=1.5)
-            i += 1
-        if "R" in col:
-            plt.plot(r,df[col],color=colors[j],linewidth=1.5)
-            j+=1
+    #plot Eigenfunctions
+    for m in tor_coupl:
+        i,j=0,0
+        d = get_colors_dict(m)
+    
+        for col in df.columns:
+            if f"/ {m}" in col or f"/{m}" in col:
+                if "I" in col:
+                    plt.plot(r,df[col],"--",color=d['colors'][i],linewidth=1.5)
+                    i += 1
+                if "R" in col:
+                    plt.plot(r,df[col],color=d['colors'][j],linewidth=1.5)
+                    j += 1
         
-    plt.title(f"EP {round(energy)} keV/ "+ r"$\beta$:"+f"{beta}+/ $f$: {round(f)} kHz",fontsize=22)               
+    plt.title(f"EP {round(energy)} keV/ "+ r"$\beta$:"+f"{beta}/ $f$: {round(f)} kHz",fontsize=22)               
     plt.xlabel("r/a",fontsize=20)
     plt.ylabel(r"$\Phi$",fontsize=20)
     plt.grid(True)
     plt.legend(loc="lower left")
-    plt.savefig(f"{sav_file}{round(energy)}_{beta}.jpg",dpi=600)
+    plt.savefig(f"{sav_file}{round(energy)}_{beta}.png",dpi=400)
 
 
 #Creates an array of sorted plots for panoramic visualization
 def eigenfunction_maps(directory):
     images = []
     
-    act_dir = os.listdir(directory)
-    act_dir = sorted(list(act_dir))
-    new_act_dirA = sorted([f.split("_")[0] for f in act_dir],reverse=True)
-    new_act_dirB = [f.split("_")[1] for f in act_dir]
-    act_dir = [new_act_dirA[i] + "_" + new_act_dirB[i] for i in range(len(act_dir))]
-    cont = [f.split("_")[0] for f in act_dir]
-    cont = cont.count(cont[0])
+    #Arrange list in descending order for energy and ascending for beta
+    act_dir = os.listdir(directory) #makes a list of the images names 
+    act_dir = sorted(act_dir)
+    act_dir = sorted(act_dir, key=sort_key,reverse=True)
     
-    # Loop through the directory and add the images to the list
+    #Gets the number of energy plots for the array of images
+    my_list = [f.split("_")[0] for f in act_dir]
+    unique_elements = sorted(set(my_list),reverse=True)
+    columns = []
+    for element in unique_elements:
+        count = my_list.count(element)
+        columns.append(count)
+        
+    column_num = max(columns)
+    row_num = len(columns)
+    
+    # Loop through the directory and add the images to a list
     for filename in act_dir:
         if filename.endswith('.jpg') or filename.endswith('.png'):
             images.append(Image.open(os.path.join(directory, filename)))
             
     # Calculate the width and height of the output image
-    output_width = int(sum([image.size[0]/2 for image in images])/cont)
-    output_height = int(cont*max([image.size[1]/2 for image in images]))
+    output_width = int(column_num*(images[0].size[0]/2))
+    output_height = int(row_num*(images[0].size[1]/2))
 
-    # Create a new output image
+    #Create a new image
     output_image = Image.new('RGB', (output_width, output_height))
     
     # Loop through the images and paste them onto the output image
-    x = 0
-    contador,i = 0,0
+    row_count,column_count = 0,0
+    num_col = 0
+    x,i = 0,0 #x:image position for each column,i:image position for each column
+    
     for image in images:
-        if contador == cont:
-            contador = 0
+        if column_count == columns[num_col]:
+            num_col += 1
+            column_count = 0
             x = 0
-            i += int(output_height/cont) 
+            i += int(output_height/row_num) 
         
         output_image.paste(image.resize((int(image.size[0]/2), 
                                          int(image.size[1]/2))), (x, i))
-        x += int(image.size[0]/2)
-        contador += 1
+        x += int(image.size[0]/2)  #Position in x for the image
+        column_count += 1    
         
-    output_image.save(directory + "Eigenfunctions_full.jpg")
+    output_image.save("Eigenfunctions_full.jpg")
 
 
 def dominant_mode_width(max_dataframe):
@@ -312,5 +379,3 @@ def dominant_mode_width(max_dataframe):
     y2 = max_mode[name_column[0]].iloc[-1]
     
     return y1,x1,y2,x2
-
-
