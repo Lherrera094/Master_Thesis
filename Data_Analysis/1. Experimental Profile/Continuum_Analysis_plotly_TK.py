2910bed5b90e4e5ac80e3cbdbd291c91c87dbcad
Luis Carlos Herrera Quesada <127136402+Lherrera094@users.noreply.github.com>
2023-06-01 17:13:41 -0600

    Add files via upload

--- Data_Analysis/1. Experimental Profile/Continuum_Analysis_plotly_TK.py
+++ Data_Analysis/1. Experimental Profile/Continuum_Analysis_plotly_TK.py
@@ -2,41 +2,41 @@
 # coding: utf-8
 # %%
 # Developer: Luis Carlos Herrera Quesada
-# Date: 27/04/2023
+# Date: 31/05/2023
 # Universidad Carlos III de Madrid
 
 # %%
-
-
 #import plotly.express as px
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 import numpy as np
+import webbrowser
+from pathlib import Path
+import os
 
 
 # %%
-
-
-#Read Alfven data
-def read_dataframe(x):
-    exp_df = pd.read_excel(f"n{x}/Output_Exp_{x}.xlsx")    #Experimental Profile
+def read_df(x,prof,fam):
     
-    OnA_df = pd.read_excel(f"{x}/Output_OnA_{x}.xlsx")    #On Axis Profile
+    nomb = "."
+    dir_image = f"{fam}/Eigenfunction_plots/"
+    image = os.listdir(dir_image)
+    localh = f"<a href=\"http://localhost:8888/view/Documents/Master/TFM/Data_Analysis/1_Experimental_Profile/{dir_image}"
+    local_host_image = [] 
     
-    OfA_df = pd.read_excel(f"{x}/Output_OfA_{x}.xlsx")    #Off Axis Profile
+    image = sorted(image, key=lambda x: (float(x.split('_')[0]), float(x.split('_')[1].split('.png')[0])))
     
-    hg_df = pd.read_excel(f"{x}/Output_HG_{x}.xlsx")      #High Gradient Profile 
+    for i in image:
+        local_host_image.append(f"{localh}{i}\">{nomb}</a>")
     
-    lg_df = pd.read_excel(f"{x}/Output_LG_{x}.xlsx")      #Low Gradient Profile
+    link_df = pd.DataFrame(local_host_image, columns=["link_to_image"])
     
-    return exp_df,OnA_df,OfA_df,hg_df,lg_df
-
-
-# %%
-def read_df(x,prof):
     df = pd.read_excel(f"n{x}/Output_{prof}_n{x}.xlsx")
+    df["link_to_image"] = link_df
     df = df.drop(df[df[f"radial_pos_maximum"] > 0.8].index)
+    
     return df
 
 
@@ -51,9 +51,9 @@ def alfv_continuum_read(x,f_max,path):
     df = df.drop(df[df[f"n={x}"] <= 10].index)
     df = df.drop(df[df[f"n={x}"] > f_max].index)
     
-    color = ["white","navy","magenta","black","firebrick","blue","darkcyan",
-              "darkgreen","royalblue","green","red","indigo","salmon","mediumpurple",
-              "dimgrey","orange"]
+    color = ["white","black","navy","darkslategrey","sandybrown","blue","darkturquoise",
+              "limegreen","springgreen","darkred","orange","darkmagenta","olive",
+             "grey","red","brown","mediumvioletred","forestgreen"]
     
     return df, color[x]
 
@@ -62,8 +62,6 @@ def alfv_continuum_read(x,f_max,path):
 
 
 def family_information(fam):
-    
-    markers = {'HAE':"o","TAE":"^",'GAE':"s"}
     
     dictionary = {
         "n7": {
@@ -100,7 +98,7 @@ def family_information(fam):
         }
     }
     
-    return markers, dictionary[fam]
+    return dictionary[fam]
 
 
 # %%
@@ -124,7 +122,7 @@ def alfv_continuum(f_max,save_file,path):
     fig.update_layout(
         title={
             'text': "Alfvén Continuum",
-            'y': 0.9,  # Adjust the vertical position of the title
+            'y': 0.95,  # Adjust the vertical position of the title
             'x': 0.5,  # Set the x position to center the title
             'xanchor': 'center',
             'yanchor': 'top',
@@ -136,21 +134,20 @@ def alfv_continuum(f_max,save_file,path):
     fig.update_layout(yaxis_range=[0,f_max])    
     fig.show()
     
+    fig.write_html(f"{save_file}/Continuum_html.html")
     fig.write_image(f"{save_file}/continuum.png",width=800, height=600, scale=3)
 
 
 # %%
-
-
-def individual_helical_plot(df,f_max,dict_fam,path):
-    title = dict_fam["title"]
+def individual_helical_plot(df,f_max,dict_fam,path,save_file,fam,image_path):
     
-    # plot the data
+    title = dict_fam["title"]
     fig = go.Figure()
     
     fig = px.scatter(df,
                      x = "radial_pos_maximum",
                      y = "f(kHz)",
+                     text = df["link_to_image"],
                      size = "width",
                      color = "Growth Rate",
                      symbol = "Alfvén_mode",
@@ -158,42 +155,107 @@ def individual_helical_plot(df,f_max,dict_fam,path):
                      labels=dict(beta = "Beta_f",
                                  radial_pos_maximum="Position",
                                  dominant_mode = "Dominant Mode"))
-
+    
+    fig.update_traces(textposition = "middle center")
+    fig.update_layout(uniformtext_minsize=4, uniformtext_mode='hide')
+    
     for i in dict_fam["continuum"]:
         df_cont,colors = alfv_continuum_read(i,f_max,path)
         fig = fig.add_trace(go.Scatter(x = df_cont["r"],
                                        y = df_cont[f"n={i}"], 
-                                        mode='markers',
+                                       mode='markers',
                                         marker=dict(
                                         size=3,
                                         color=colors   
                                         ),
                                         name = f"n={i}"))
+    
     fig.update_layout(
                 hoverlabel=dict(
-                bgcolor="white",
-                font_size=16,
+                bgcolor="bisque",
+                font_size=14,
                 font_family="Rockwell"
                             )
                         )
+    
     fig.update_coloraxes(showscale=False)
     
     fig.update_layout(
         title={
             'text': f"Alfvén Continuum {title}",
-            'y': 1.0,  # Adjust the vertical position of the title
+            'y': 0.95,  # Adjust the vertical position of the title
             'x': 0.5,  # Set the x position to center the title
             'xanchor': 'center',
             'yanchor': 'top',
             'font': dict(size=24)  # Customize the font size of the title
-        }
+        },
+        font=dict(size=15),
     )
+    
     fig.update_xaxes(title_text='r/a',title_font = {"size": 24})
     fig.update_yaxes(title_text='Frequency (kHz)',title_font = {"size": 24})
     fig.update_layout(yaxis_range=[0,300])
-    fig.update_layout(xaxis_range=[0,1])
+    fig.update_layout(xaxis_range=[0.1,1.1])
+    
     fig.show()
-    fig.write_html("Experimental profile.html")
+    fig.write_html(f"{save_file}/Experimental_profile_{title}.html")
 
 
 # %%
+def maximum_instability(df5,df6,df7):
+    
+    col_drop = ["dominant_mode","radial_pos_maximum","coupling","link_to_image"]
+    
+    E_ep_n5, beta_n5, gr5 = get_max_gr(df5)
+    E_ep_n6, beta_n6, gr6 = get_max_gr(df6)
+    E_ep_n7, beta_n7, gr7 = get_max_gr(df7)
+    
+    max_gr = [gr5,gr6,gr7]
+    
+    maximum = check_max_value(max_gr)
+    
+    if maximum == gr5:
+        E_ep_h = E_ep_n5
+    
+    elif max_gr == gr6:
+        E_ep_h = E_ep_n6
+    
+    else:
+        E_ep_h = E_ep_n7
+    
+    df_new_n5 = df5.drop(df5[df5['efast'] != E_ep_h].index)
+    df_new_n6 = df6.drop(df6[df6['efast'] != E_ep_h].index)
+    df_new_n7 = df7.drop(df7[df7['efast'] != E_ep_h].index)
+    
+    # Drop multiple columns
+    col_drop = ["dominant_mode","radial_pos_maximum","coupling","link_to_image"]
+    
+    df_new_n5 = df_new_n5.drop(col_drop, axis=1)
+    df_new_n6 = df_new_n6.drop(col_drop, axis=1)
+    df_new_n7 = df_new_n7.drop(col_drop, axis=1)
+    
+    return df_new_n5, df_new_n6, df_new_n7, E_ep_h
+
+
+# %%
+def get_max_gr(df):
+    #Gets row for the simulation with larger growth rate
+    gr_index = df[df["Growth Rate"] == df["Growth Rate"].max()].index
+    
+    gr = df.loc[gr_index]["Growth Rate"].values[0]
+    E_ep = df.loc[gr_index]["efast"].values[0]
+    beta = df.loc[gr_index]["beta"].values[0]
+    
+    return E_ep, beta, gr
+
+
+# %%
+def check_max_value(n_array):
+    
+    maximum = n_array[0]  # Initialize the maximum as the first element
+    
+    for num in n_array:
+        if num > maximum:
+            maximum = num
+            
+    return maximum
